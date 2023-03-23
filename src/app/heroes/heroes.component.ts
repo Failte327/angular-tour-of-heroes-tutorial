866f8ee1bd5bedc948d8238d478a8b47ac34b1bb
Timothy Twelker <timothy.twelker@securitymetrics.com>
2023-03-23 11:20:22 -0600

    Step 5/6 complete

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,17 +9,8 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
 
   heroes: Hero[] = [];
-  selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
-  }
 
   constructor(private heroService: HeroService, private messageService: MessageService) {
 
